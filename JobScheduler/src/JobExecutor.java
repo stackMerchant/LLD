c08e8f82dd8d956498af9f00b7cb916f8eee48c8
Somnath Vats <somnathvats@Somnaths-MacBook-Pro-2.local>
2025-04-18 01:10:38 +0530

    Refactor job scheduler code

--- JobScheduler/src/JobExecutor.java
+++ JobScheduler/src/JobExecutor.java
@@ -1,10 +1,21 @@
 public class JobExecutor {
 
-    JobExecutor(int workerCount, JobQueue jobQueue) {
-        for (int i = 0; i < workerCount; i++) {
-            Worker worker = new Worker(i + 1, jobQueue);
-            new Thread(worker).start();
+    JobExecutor(int _workerCount, JobQueue _jobQueue) {
+
+        Runnable runnableWorker = () -> {
+            while (true) {
+                try {
+                    Job job = _jobQueue.pop(); // Get job
+                    job.task.run(); // Do job
+                    System.out.println(Thread.currentThread().getName() + " completed job: " + job.jobId);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        for (int i = 0; i < _workerCount; i++) {
+            new Thread(runnableWorker).start();
         }
     }
-
 }
